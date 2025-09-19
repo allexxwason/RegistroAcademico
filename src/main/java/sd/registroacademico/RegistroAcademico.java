7d0fc090d211635aba7c2de0efc0a53a4a1b91c1
alex vasquez <alexvb1419@gmail.com>
2025-09-19 17:01:37 -0500

    Primer commit - proyecto RegistroAcademico

---
+++ src/main/java/sd/registroacademico/RegistroAcademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sd.registroacademico;
+
+/**
+ *
+ * @author Coder
+ */
+public class RegistroAcademico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
